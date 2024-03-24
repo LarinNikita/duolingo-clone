8bdf8a975ad27f5fd1899cb7a34092fcda94f984
LarinNikita <larin_nikita_m@mail.ru>
2024-03-24 16:13:23 +0700

    clerk setup and marketing header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Nunito } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
 
 import type { Metadata } from "next";
 
@@ -18,8 +19,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={font.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={font.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
