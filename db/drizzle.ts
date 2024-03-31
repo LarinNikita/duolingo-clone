1861ce7c7ff0798987ffc5ad671cb1a99156cbe2
LarinNikita <larin_nikita_m@mail.ru>
2024-03-31 16:54:55 +0700

    courses page and fix connection string to db

--- db/drizzle.ts
+++ db/drizzle.ts
@@ -1,7 +1,7 @@
 import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
 
-import * as schema from "./schema"
+import * as schema from './schema';
 
 const sql = postgres(process.env.DATABASE_URL!)
 const db = drizzle(sql, { schema });
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,9 @@
 import 'dotenv/config';
 import type { Config } from 'drizzle-kit';
 
-
 export default {
     schema: "./db/schema.ts",
-    out: "./drizzle",
+    out: "./drizzle.ts",
     driver: "pg",
     dbCredentials: {
         connectionString: process.env.DATABASE_URL!,
